100ba2608ca6de32561aee19385f79f745d9ac86
patrocle <julien.petit.mathay@gmail.com>
2012-10-19 11:32:53 +0200

    Myitemized

---
+++ src/com/android/activity/MyItimizedOverlay.java
@@ -0,0 +1,18 @@
+package com.android.activity;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.OverlayItem;
+
+public class MyItimizedOverlay {
+
+	public MyItimizedOverlay(Drawable drawable) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void addOverlayItem(OverlayItem overlayitem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
