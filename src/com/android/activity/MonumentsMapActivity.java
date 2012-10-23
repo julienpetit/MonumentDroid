3d439e69eb9d331d4a881a5bfa1b29eb07f03c2f
Pierrick <pierrick.tyrode@gmail.com>
2012-10-23 14:59:22 +0200

    Lion de Belfort

--- src/com/android/activity/MonumentsMapActivity.java
+++ src/com/android/activity/MonumentsMapActivity.java
@@ -30,60 +30,43 @@ public class MonumentsMapActivity extends MapActivity implements LocationListene
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		
-//		setContentView(R.layout.mapmonuments);
-//
-//
-//		// On récupère les coordonnées GPS
-//		/// On récupère le gestionnaire de localisation
-//		LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-//		/// On définit les critère de choix du meilleur système de localisation
-//		Criteria criteria = new Criteria();
-//		criteria.setAccuracy(Criteria.ACCURACY_FINE);
-//		criteria.setAltitudeRequired(true);
-//		criteria.setBearingRequired(false);
-//		criteria.setCostAllowed(true);
-//		criteria.setPowerRequirement(Criteria.POWER_LOW);
-//		String provider = lm.getBestProvider(criteria, true);
-//		/// On demande au gestionnaire de localisation de faire des mise-a-jour de la position.
-//		lm.requestLocationUpdates(provider, update_time, update_distance, this);
-//		/// On récupère la dernière position connu
-//		this.location = lm.getLastKnownLocation(provider);
-//		
-//		Drawable drawable = this.getResources().getDrawable(R.drawable.ic_launcher);
-//		MyItimizedOverlay itemizedoverlay = new MyItimizedOverlay(drawable);
-//		GeoPoint geoPoint = new GeoPoint(-17595983, -149487411);
-//		OverlayItem overlayitem = new OverlayItem(geoPoint, "Hello from", "Tahiti");
-//		itemizedoverlay.addOverlayItem(overlayitem);
-//		List<Overlay> mapOverlays = mapView.getOverlays();
-//		mapOverlays.add(itemizedoverlay);
-		
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.mapmonuments);
 
-		
+		// On récupère les coordonnées GPS
+		/// On récupère le gestionnaire de localisation
+		LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		/// On définit les critère de choix du meilleur système de localisation
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_FINE);
+		criteria.setAltitudeRequired(true);
+		criteria.setBearingRequired(false);
+		criteria.setCostAllowed(true);
+		criteria.setPowerRequirement(Criteria.POWER_LOW);
+		String provider = lm.getBestProvider(criteria, true);
+		/// On demande au gestionnaire de localisation de faire des mise-a-jour de la position.
+		lm.requestLocationUpdates(provider, update_time, update_distance, this);
+		/// On récupère la dernière position connu
+		this.location = lm.getLastKnownLocation(provider);
+
 		mapView = (MapView) this.findViewById(R.id.map);
 		mapView.setBuiltInZoomControls(true);
-		
+
 		Drawable drawable = this.getResources().getDrawable(R.drawable.ic_launcher);
-		MyItimizedOverlay itemizedoverlay = new MyItimizedOverlay(drawable,this);
-		
-		GeoPoint geoPoint = new GeoPoint(-17595983, -149487411);
-		OverlayItem overlayitem = new OverlayItem(geoPoint, "Hello from", "Tahiti");
+		//MyItimizedOverlay itemizedoverlay = new MyItimizedOverlay(drawable);
+		MyItimizedOverlay itemizedoverlay = new MyItimizedOverlay(drawable,mapView);
+
+		GeoPoint geoPoint = new GeoPoint((int)((47.637942)*1E6),(int)((6.862813) * 1E6));
+		OverlayItem overlayitem = new OverlayItem(geoPoint, "Lion de Belfort", "All�e du Souvenir Fran�ais, 90000 Belfort");
 		itemizedoverlay.addOverlayItem(overlayitem);
-		
-		GeoPoint geoPoint2 = new GeoPoint(-17528941, -149826891);
-		OverlayItem overlayitem2 = new OverlayItem(geoPoint2, "Hello from", "Moorea");
-		itemizedoverlay.addOverlayItem(overlayitem2);
-		
+
 		List<Overlay> mapOverlays = mapView.getOverlays();
 		mapOverlays.add(itemizedoverlay);
-		
+
 		mapController = mapView.getController();
 		mapController.setCenter(geoPoint);
-		mapController.setZoom(10);
+		mapController.setZoom(10);	
 	}
 
 
